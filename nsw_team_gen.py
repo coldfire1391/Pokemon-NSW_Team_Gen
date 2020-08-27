f61ddcb5d7cd5ddf60bfd44cf03f7d5957a0a912
Isaiah Rae <43140411+coldfire1391@users.noreply.github.com>
2020-08-27 13:43:02 +0800

    Updated code
    Allows for choosing team size and multiple-type input

--- nsw_team_gen.py
+++ nsw_team_gen.py
@@ -22,38 +22,61 @@ TTN = {"normal":0, "fighting":1, "flying":2, "poison":3, "ground":4, "rock":5, "
 
 
 
-def comp_check(swm, fmem):
-	return not (swm[fmem[1]][fmem[0]] | swm[fmem[2]][fmem[0]] | swm[fmem[3]][fmem[0]] | swm[fmem[4]][fmem[0]] | swm[fmem[2]][fmem[1]] | swm[fmem[3]][fmem[1]] | swm[fmem[4]][fmem[1]] | swm[fmem[3]][fmem[2]] | swm[fmem[4]][fmem[2]] | swm[fmem[4]][fmem[3]])
+# def comp_check(swm, fmem):
+# 	return not (swm[fmem[1]][fmem[0]] | swm[fmem[2]][fmem[0]] | swm[fmem[3]][fmem[0]] | swm[fmem[4]][fmem[0]] | swm[fmem[2]][fmem[1]] | swm[fmem[3]][fmem[1]] | swm[fmem[4]][fmem[1]] | swm[fmem[3]][fmem[2]] | swm[fmem[4]][fmem[2]] | swm[fmem[4]][fmem[3]])
 
-def powerset(swm, fmemlist, s):
-	x = len(s)
+def ccz(swm, fmem, x, y):
+	if x == 1:
+		return swm[fmem[x]][fmem[0]]
+	elif y == -1:
+		return ccz(swm, fmem, x-1, x-2)
+	else:
+		return (swm[fmem[x]][fmem[y]] | ccz(swm, fmem, x, y-1))
+
+def powerset(swm, pmem, lenm, ts, fmemlist):
+	x = len(pmem)
 	for i in range(1 << x):
-		Fmem = [s[j] for j in range(x) if (i & (1 << j))]
-		if len(Fmem) == 5 and comp_check(swm, Fmem):
-			# print(Fmem)
-			fmemlist.append(Fmem)
+		Fmem = [pmem[j] for j in range(x) if (i & (1 << j))]
+		if len(Fmem) == (ts-lenm) and not ccz(swm,Fmem,len(Fmem)-1,len(Fmem)-2):
+				fmemlist.append(Fmem)
 	return
 
 
 
 SWM = pd.read_csv("SWM.csv").to_numpy() # it is now a matrix
-# print(SWM)
 
+# creates and writes to the output file
 f = open("output.txt", "w")
 
 cont = "y"
 while cont == "y":
-	# mandatory type <static variable>
-	mtype = TTN[input("Please input one Type you wish to have on your team: ").lower()]
-	f.write(NTT[mtype]+"\n")
+	# team size
+	tsize = int(input("Input desired team size: "))
+
+	# mandatory type <array>
+	Mtypes = sorted([TTN[t] for t in input("Input the types you wish to have on your team, separated by comma ','\n (6 MAX): ").lower().split(",")])
+	# print("Mtypes: ",Mtypes)
+	
+	# obtain length of Mtypes
+	len_m = len(Mtypes)
+	# print("len_m: ",len_m)
+
+	# extend length of Mtypes to 6
+	while len(Mtypes) < 6:
+		Mtypes.append(Mtypes[0])
+	# print("Mtypes: ",Mtypes)
+
+	for x in range(len_m):
+		f.write(NTT[Mtypes[x]]+" ")
+	f.write("\n")
 
 	# potential members <array>
-	Pmem = [p for p in range(0,18) if SWM[mtype][p] == 0] # or p == mtype
+	Pmem = [p for p in range(0,18) if (SWM[Mtypes[0]][p] == 0 | SWM[Mtypes[1]][p] == 0 | SWM[Mtypes[2]][p] == 0 | SWM[Mtypes[3]][p] == 0 | SWM[Mtypes[4]][p] == 0 | SWM[Mtypes[5]][p] == 0)] # or p == mtype
 	print(Pmem)
 
 	FmemList = []
 
-	powerset(SWM, FmemList, Pmem)
+	powerset(SWM, Pmem, len_m, tsize, FmemList)
 	final_output = [str([NTT[z] for z in w])+"\n" for w in FmemList]
 
 	f.writelines(final_output)
